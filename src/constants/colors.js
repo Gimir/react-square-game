15279005ab2e0486c6a03bb7f259bb56fd9909c0
Gimir <mukhammad.amaev@mail.ru>
2020-02-14 16:35:20 +0300

    Add colors constant
    Create constant directory and add colors.js file

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export default =  {
+    green: '#00E871',
+    blue: '#42D8E8',
+    red: '#E85A5F',
+    buttonBackground: '#7B8D93',
+    secondaryBackground: '#F3F3F3',
+    primaryBackground: '#CFD8DC',
+    secondaryText: '#9FA8AB',
+    primaryText: '#888888'
+}
