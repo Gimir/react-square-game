f2e6f715b85acd8ec8c9fa5d140944968e03dcb1
Gimir <mukhammad.amaev@mail.ru>
2020-02-14 22:20:09 +0300

    Add synchronous action creators
    Added all syncronous action creators

---
+++ src/action_creators/syncActions.js
@@ -0,0 +1,38 @@
+import '../constants/actions';
+
+export const setGameStatus = status => ({
+    type: SET_GAME_STATUS,
+    payload: status
+});
+
+export const setGameModes = modes => ({
+    type: SET_GAME_MODES,
+    payload: modes
+});
+
+export const setCurrentMode = mode => ({
+    type: SET_CURRENT_MODE,
+    payload: mode
+});
+
+export const setUsername = name => ({
+    type: SET_USERNAME,
+    payload: name
+});
+
+export const setLiderBoard = board => ({
+    type: SET_LIDER_BOARD,
+    payload: board
+});
+
+export const increaseAICount = () => ({
+    type: INCREASE_AI_COUNT
+});
+
+export const increaseUserCount = () => ({
+    type: INCREASE_USER_COUNT
+});
+
+export const resetGame = () => ({
+    type: RESET_GAME
+});
