eb977808e981297370a6aa34bd8d0fab0b8a99eb
Gimir <mukhammad.amaev@mail.ru>
2020-02-15 21:51:58 +0300

    Add simple grid
    Add two columns - app--content, app-sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import MenuContainer from './containers/MenuContainer';
+
 function App() {
   return (
     <div className="App">
-      
+      <div className="app--content">
+        <MenuContainer />
+      </div>
+      <div className="app--sidebar">
+
+      </div>
     </div>
   );
 }
