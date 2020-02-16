2cb638d72dd34428f6f8d3a42f59c4e799daba60
Gimir <mukhammad.amaev@mail.ru>
2020-02-16 10:51:27 +0300

    Import GameContainer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import MenuContainer from './containers/MenuContainer';
 import RaitingContainer from './containers/RaitingContainer';
+import GameContainer from './containers/GameContainer';
 
 function App() {
   return (
     <div className="App">
       <div className="app--content">
         <MenuContainer />
+        <GameContainer />
       </div>
       <div className="app--sidebar">
         <RaitingContainer />
