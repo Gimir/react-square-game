0afd9c00bf7ec6263315b13f620657950420a166
Gimir <mukhammad.amaev@mail.ru>
2020-02-17 11:46:20 +0300

    Separate side functions

---
+++ src/helpers/index.js
@@ -0,0 +1,17 @@
+export const getDate = () => {
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    const time = today.getHours() + ':' + today.getMinutes();
+    today = mm + '/' + dd + '/' + yyyy;
+
+    return `${today} ${time}`;
+}
+
+export const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
